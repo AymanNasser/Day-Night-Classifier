e783d486f5967c036561ecf31068b72e351b168a
AymanNasser <aymannasr43@gmail.com>
2021-02-12 18:55:13 +0200

    Type: feat Added data generator that returns data as csv file
    Signed-off-by: AymanNasser <aymannasr43@gmail.com>

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -9,12 +9,15 @@ class DayNightDataset(Dataset):
     def __init__(self, root_dir = os.getcwd() + '/dataset', transform=None):
         if not os.path.exists(root_dir):
             raise OSError("Wrong Path for Dataset")
+
         self.root_dir = root_dir
         self.transform = transform
 
          
     def __len__(self):
-        return len(os.listdir(self.root_dir + 'training'))
+        day_imgs = len(os.listdir(self.root_dir + 'training/day'))
+        night_imgs = len(os.listdir(self.root_dir + 'training/night'))
+        return day_imgs + night_imgs
 
     def __getitem__(self, index):
         pass
